e860a8b007adb2d97da92cd61f164413570f16d4
Ryan Viajedor <ryanmoradasviajedor@gmail.com>
2025-07-08 23:57:24 +0300

    SwiftUI Button Style

--- Hike/Component/CardVC.swift
+++ Hike/Component/CardVC.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct CardVC: View {
+    // MARK: - PROPERTIES
+    
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    // MARK: - FUNCTIONS
+    
+    func randomImage() {
+        print("--- BUTTON WAS PRESSED ---")
+        print("Status: Old Image Number: \(imageNumber)")
+        
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+            print("Action: Random Number Generated: \(randomNumber)")
+        } while randomNumber == imageNumber
+        
+        imageNumber = randomNumber
+        print("Result: New Image Number: \(imageNumber)")
+        print("--- THE END ---")
+        print("\n")
+    }
+    
     var body: some View {
         // MARK: - CARD
+        
         ZStack {
             CustomBackgroundView()
             
@@ -66,13 +89,38 @@ struct CardVC: View {
                             )
                         ).frame(width: 256, height: 256)
                     
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
+                        .animation(
+                            .default,
+                            value: imageNumber
+                        )
                 }
                 
                 // MARK: - FOOTER
-            }   //: VSTACK
+                
+                Button {
+                    // ACTION: Generate a ram=ndom number
+                    randomImage()
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [
+                                    .customGreenLight,
+                                    .customGreenMedium
+                                ],
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                        .shadow(color:.black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+                }
+                .buttonStyle(GradientButton())
+            }   //: ZSTACK
         } //: CARD
         .frame(width: 320, height: 570)
     }
